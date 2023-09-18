f0e3f21ba0e827649372b075dd8834d7a7160ad8
lalywr2000 <12190450@inha.edu>
2023-09-18 13:14:45 +0200

    #11 using Py_INCREF

--- piracer_client/src/PiracerClient/piracerClass.cpp
+++ piracer_client/src/PiracerClient/piracerClass.cpp
@@ -6,14 +6,19 @@ PiracerClass::PiracerClass()
 {
     Py_Initialize();
     pModule = PyImport_ImportModule("piracer.vehicles");
+    Py_INCREF(pModule);
     pClass = PyObject_GetAttrString(pModule, "PiRacerStandard");
+    Py_INCREF(pClass);
     pInstance = PyObject_CallObject(pClass, NULL);
+    Py_INCREF(pInstance);
 }
 
 uint16_t PiracerClass::getBatteryLevel()
 {
     pResult = PyObject_CallMethod(pInstance, "get_battery_voltage", NULL);
+    Py_INCREF(pResult);
     voltageLevel = PyFloat_AsDouble(pResult);
+    Py_DECREF(pResult);
     batteryLevel = (uint16_t)((voltageLevel - 2.8 * 3.0) / (12.3 - 2.8 * 3.0) * 100.0);
     
     return batteryLevel;
@@ -21,6 +26,9 @@ uint16_t PiracerClass::getBatteryLevel()
        
 PiracerClass::~PiracerClass()
 {
+    Py_DECREF(pInstance);
+    Py_DECREF(pClass);
+    Py_DECREF(pModule);
     Py_Finalize();
 }
 
