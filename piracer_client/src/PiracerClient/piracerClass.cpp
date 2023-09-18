8224908503fce9d645c3aafa611a0682b9a52cf0
lalywr2000 <12190450@inha.edu>
2023-09-18 14:12:30 +0200

    #11 piracer class fix

--- piracer_client/src/PiracerClient/piracerClass.cpp
+++ piracer_client/src/PiracerClient/piracerClass.cpp
@@ -6,29 +6,24 @@ PiracerClass::PiracerClass()
 {
     Py_Initialize();
     pModule = PyImport_ImportModule("piracer.vehicles");
-    Py_INCREF(pModule);
     pClass = PyObject_GetAttrString(pModule, "PiRacerStandard");
-    Py_INCREF(pClass);
     pInstance = PyObject_CallObject(pClass, NULL);
-    Py_INCREF(pInstance);
 }
 
 uint16_t PiracerClass::getBatteryLevel()
 {
     pResult = PyObject_CallMethod(pInstance, "get_battery_voltage", NULL);
-    Py_INCREF(pResult);
     voltageLevel = PyFloat_AsDouble(pResult);
-    Py_DECREF(pResult);
     batteryLevel = (uint16_t)((voltageLevel - 2.8 * 3.0) / (12.3 - 2.8 * 3.0) * 100.0);
     
     return batteryLevel;
 }
-       
+
 PiracerClass::~PiracerClass()
 {
-    Py_DECREF(pInstance);
-    Py_DECREF(pClass);
-    Py_DECREF(pModule);
+    Py_XDECREF(pInstance);
+    Py_XDECREF(pClass);
+    Py_XDECREF(pModule);
     Py_Finalize();
 }
 
