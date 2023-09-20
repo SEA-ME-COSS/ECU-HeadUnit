adb0d5c93f9c8836d0f9074c22c912b1f21c5acf
lalywr2000 <12190450@inha.edu>
2023-09-20 18:19:02 +0200

    #11 fix

--- gear_control/src/PiracerOperator/takeControlThread.cpp
+++ gear_control/src/PiracerOperator/takeControlThread.cpp
@@ -5,12 +5,12 @@
 using namespace v1_0::commonapi;
 
 void *takeControlThread(void *arg) {
-    //std::shared_ptr<CommonAPI::Runtime> runtime;
-    //std::shared_ptr<PiracerOperatorStubImpl> PiracerOperatorService;
+    std::shared_ptr<CommonAPI::Runtime> runtime;
+    std::shared_ptr<PiracerOperatorStubImpl> PiracerOperatorService;
     
-    //runtime = CommonAPI::Runtime::get();
-    //PiracerOperatorService = std::make_shared<PiracerOperatorStubImpl>();
-    //runtime->registerService("local", "PiracerOperator", PiracerOperatorService);
+    runtime = CommonAPI::Runtime::get();
+    PiracerOperatorService = std::make_shared<PiracerOperatorStubImpl>();
+    runtime->registerService("local", "PiracerOperator", PiracerOperatorService);
     
     double throttle, steering;
     
