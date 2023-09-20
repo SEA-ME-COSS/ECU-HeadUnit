f0553aa4ba2c6a1368462650dc0c4cd43b9bd722
lalywr2000 <12190450@inha.edu>
2023-09-20 17:59:35 +0200

    #11 fix

--- gear_control/src/PiracerOperator/PiracerOperator.cpp
+++ gear_control/src/PiracerOperator/PiracerOperator.cpp
@@ -2,22 +2,19 @@
 #include <pthread.h>
 
 // Include Thread header
-//#include "sendBatteryThread.hpp"
+#include "sendBatteryThread.hpp"
 //#include "takeControlThread.hpp"
-#include "PiracerClass.hpp"
-#include <iostream>
 
 
 int main (int argc, char **argv)
 {
     // Create threads for battery and control
-    //pthread_t batteryThread, controlThread;
-    //pthread_create(&batteryThread, NULL, sendBatteryThread, NULL);
+    pthread_t batteryThread // controlThread;
+    pthread_create(&batteryThread, NULL, sendBatteryThread, NULL);
     //pthread_create(&controlThread, NULL, takeControlThread, NULL);
 
-    //pthread_join(batteryThread, NULL);
+    pthread_join(batteryThread, NULL);
     //pthread_join(controlThread, NULL);
-    std::cout << "hasdfaefaf" << std::endl;
 
     return 0;
 }
