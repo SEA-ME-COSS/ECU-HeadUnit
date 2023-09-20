2dd41dd4e602a0041777c80f84c2d9f0735a9a52
lalywr2000 <12190450@inha.edu>
2023-09-20 16:45:03 +0200

    #11 fix

--- gear_control/src/PiracerOperator/PiracerOperator.cpp
+++ gear_control/src/PiracerOperator/PiracerOperator.cpp
@@ -9,9 +9,9 @@
 int main (int argc, char **argv)
 {
     // Create threads for battery and control
-    pthread_t batteryThread, controlThread;
-    pthread_create(&batteryThread, NULL, sendBatteryThread, NULL);
-    pthread_create(&controlThread, NULL, takeControlThread, NULL);
+    //pthread_t batteryThread, controlThread;
+    //pthread_create(&batteryThread, NULL, sendBatteryThread, NULL);
+    //pthread_create(&controlThread, NULL, takeControlThread, NULL);
 
     //pthread_join(batteryThread, NULL);
     //pthread_join(controlThread, NULL);
