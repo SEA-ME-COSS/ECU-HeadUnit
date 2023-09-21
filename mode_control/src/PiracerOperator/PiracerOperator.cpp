70e83929a48b870b8b2963c084503d771772fa8d
lalywr2000 <12190450@inha.edu>
2023-09-21 12:06:07 +0200

    #11 fixing segmentation error

--- mode_control/src/PiracerOperator/PiracerOperator.cpp
+++ mode_control/src/PiracerOperator/PiracerOperator.cpp
@@ -9,12 +9,12 @@
 int main (int argc, char **argv)
 {
     // Create threads for battery and control
-    pthread_t batteryThread, controlThread;
+    pthread_t batteryThread;  // , controlThread;
     pthread_create(&batteryThread, NULL, sendBatteryThread, NULL);
-    pthread_create(&controlThread, NULL, takeControlThread, NULL);
+    //pthread_create(&controlThread, NULL, takeControlThread, NULL);
 
     pthread_join(batteryThread, NULL);
-    pthread_join(controlThread, NULL);
+    //pthread_join(controlThread, NULL);
 
     return 0;
 }
