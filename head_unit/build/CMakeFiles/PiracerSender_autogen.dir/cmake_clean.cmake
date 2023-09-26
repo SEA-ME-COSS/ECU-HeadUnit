8bea75caad47f291b3b2eb6411566fd22a8a7556
lalywr2000 <12190450@inha.edu>
2023-09-26 11:02:00 +0200

    #12 delete build file

--- head_unit/build/CMakeFiles/PiracerSender_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/PiracerSender_autogen"
-  "PiracerSender_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PiracerSender_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
