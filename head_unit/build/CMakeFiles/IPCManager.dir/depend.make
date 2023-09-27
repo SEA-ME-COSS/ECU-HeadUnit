36adf5654c0d11dfb94f95b828ed3f64183a67f7
lalywr2000 <12190450@inha.edu>
2023-09-27 13:58:39 +0200

    #12 testing class instance change

---
+++ head_unit/build/CMakeFiles/IPCManager.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for IPCManager.
+# This may be replaced when dependencies are built.
--- head_unit/src/IPCManager/IPCManagerStubImpl.cpp
+++ head_unit/src/IPCManager/IPCManagerStubImpl.cpp
@@ -4,10 +4,23 @@
 IPCManagerStubImpl::IPCManagerStubImpl() { }
 IPCManagerStubImpl::~IPCManagerStubImpl() { }
 
+int temp = 0;
+int wait = 0;
+
 void IPCManagerStubImpl::setSensorRpm(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _sensorRpm, setSensorRpmReply_t _reply)
 {
     sender.InstrumentClusterTargetProxy->setSpeedRpm(_sensorRpm, sender.callStatus, sender.returnMessage);
     sender.HeadUnitTargetProxy->setSensorRpm(_sensorRpm, sender.callStatus, sender.returnMessage);
+    sender.PiracerOperatorTargetProxy->setGearMode(temp, sender.callStatus, sender.returnMessage);
+    
+    wait++;
+    if (wait == 10)
+    {
+        temp++;
+        wait = 0;
+    }
+    if (temp == 4)
+        temp = 0;
 
     _reply("");
 
