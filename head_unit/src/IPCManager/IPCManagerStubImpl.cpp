3b235a41372906d963ad00211c0847804400ac71
lalywr2000 <12190450@inha.edu>
2023-09-27 15:39:28 +0200

    #12 testing class instance change

--- head_unit/src/IPCManager/IPCManagerStubImpl.cpp
+++ head_unit/src/IPCManager/IPCManagerStubImpl.cpp
@@ -1,28 +1,13 @@
 #include "IPCManagerStubImpl.hpp"
-#include <iostream>
 
 
 IPCManagerStubImpl::IPCManagerStubImpl() { }
 IPCManagerStubImpl::~IPCManagerStubImpl() { }
 
-int temp = 0;
-int wait = 0;
-
 void IPCManagerStubImpl::setSensorRpm(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _sensorRpm, setSensorRpmReply_t _reply)
 {
-    std::cout<<"sensorRpm: "<<_sensorRpm<<std::endl;
     sender.InstrumentClusterTargetProxy->setSpeedRpm(_sensorRpm, sender.callStatus, sender.returnMessage);
     sender.HeadUnitTargetProxy->setSensorRpm(_sensorRpm, sender.callStatus, sender.returnMessage);
-    sender.PiracerOperatorTargetProxy->setGearMode(temp, sender.callStatus, sender.returnMessage);
-    
-    wait++;
-    if (wait == 10)
-    {
-        temp++;
-        wait = 0;
-    }
-    if (temp == 4)
-        temp = 0;
 
     _reply("");
 
@@ -31,7 +16,6 @@ void IPCManagerStubImpl::setSensorRpm(const std::shared_ptr<CommonAPI::ClientId>
 
 void IPCManagerStubImpl::setBatteryLevel(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _batteryLevel, setBatteryLevelReply_t _reply)
 {
-    std::cout<<"batteryLevel: "<<_batteryLevel<<std::endl;
     sender.InstrumentClusterTargetProxy->setBattery(_batteryLevel, sender.callStatus, sender.returnMessage);
     
     _reply("");
