07755b882065d0809de581d24ec93eb3a72453fc
lalywr2000 <12190450@inha.edu>
2023-09-30 01:31:06 +0200

    #12 organizing qml files in qml folder

--- head_unit/src/HeadUnit/HeadUnit.cpp
+++ head_unit/src/HeadUnit/HeadUnit.cpp
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     
     engine.rootContext()->setContextProperty("carinfo", &carinfo);
     
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl)
     {
