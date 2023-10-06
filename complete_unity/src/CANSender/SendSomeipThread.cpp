d8b1ca981289f5fcab8b69130f268a8f0278a171
lalywr2000 <12190450@inha.edu>
2023-10-06 15:13:13 +0200

    #18 starting rebooting system

---
+++ complete_unity/src/CANSender/SendSomeipThread.cpp
@@ -0,0 +1,41 @@
+#include "SendSomeipThread.hpp"
+
+
+using namespace v1_0::commonapi;
+
+void *SendSomeipThread(void *arg)
+{
+    std::shared_ptr<CommonAPI::Runtime> runtime;
+    std::shared_ptr<IPCManagerProxy<>> IPCManagertargetProxy;
+    
+    runtime = CommonAPI::Runtime::get();
+    IPCManagertargetProxy = runtime->buildProxy<IPCManagerProxy>("local", "IPCManager");
+    
+    CommonAPI::CallStatus callStatus;
+    std::string returnMessage;
+    
+    uint16_t temp = 0;
+    
+    while (1)
+    {
+        pthread_mutex_lock(&CANBufferMutex);
+        int currentIndex = CANBufferIndex - 1;
+        if (currentIndex < 0)
+        {
+            currentIndex = CANBuffer_SIZE - 1;
+        }
+        uint16_t speed_sensor_rpm = CANBuffer[currentIndex];
+        pthread_mutex_unlock(&CANBufferMutex);
+        
+        uint16_t kf_speed_sensor_rpm = filter(speed_sensor_rpm);
+	//IPCManagertargetProxy->setSensorRpm(kf_speed_sensor_rpm, callStatus, returnMessage);
+	IPCManagertargetProxy->setSensorRpm(temp, callStatus, returnMessage);
+	temp += 100;
+	if (temp == 200)
+	    temp = 0;
+        usleep(5000000);
+    }
+    
+    return NULL;
+}
+
---
+++ complete_unity/src/IPCManager/IPCManager.cpp
@@ -0,0 +1,35 @@
+#include <unistd.h>
+#include <cstdlib>
+#include <CommonAPI/CommonAPI.hpp>
+
+#include "IPCManagerStubImpl.hpp"
+#include "IPCManagerSenderClass.hpp"
+
+
+using namespace v1_0::commonapi;
+
+int main()
+{
+    std::shared_ptr<CommonAPI::Runtime> runtime;
+    std::shared_ptr<IPCManagerStubImpl> IPCManagerService;
+    
+    runtime = CommonAPI::Runtime::get();
+    IPCManagerService = std::make_shared<IPCManagerStubImpl>();
+    runtime->registerService("local", "IPCManager", IPCManagerService);
+    
+    IPCManagerSenderClass sender;
+    int exitCode;
+    
+    while (1)
+    {
+        if (!sender.InstrumentClusterTargetProxy->isAvailable())
+	    exitCode = system("sh ../reboot_script/reboot_instrument_cluster.sh");
+        if (!sender.HeadUnitTargetProxy->isAvailable())
+	    exitCode = system("sh ../reboot_script/reboot_head_unit.sh");
+	
+	usleep(100000);
+    }
+    
+    return 0;
+}
+
