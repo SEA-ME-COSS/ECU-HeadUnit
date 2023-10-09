05818bec58cf49a13593699882e3694b53364c8f
lalywr2000 <12190450@inha.edu>
2023-10-09 15:34:09 +0200

    #18 auto reboot piracer sender

--- complete_unity/build/CMakeFiles/InstrumentCluster.dir/InstrumentCluster_autogen/34HRDSO23Q/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/InstrumentCluster.dir/InstrumentCluster_autogen/34HRDSO23Q/qrc_qml.cpp.o: \
- /home/ha/바탕화면/DES-Head-Unit/complete_unity/build/InstrumentCluster_autogen/34HRDSO23Q/qrc_qml.cpp \
- /usr/include/stdc-predef.h
--- head_unit/build/CMakeFiles/PiracerOperator.dir/PiracerOperator_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PiracerOperator.dir/PiracerOperator_autogen/mocs_compilation.cpp.o: \
- /home/ha/바탕화면/DES-Head-Unit/head_unit/build/PiracerOperator_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
