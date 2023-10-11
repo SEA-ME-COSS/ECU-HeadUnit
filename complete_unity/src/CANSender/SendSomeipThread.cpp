c9decbf7a0ea71ef46eb5bc651de2551c6e07766
lalywr2000 <12190450@inha.edu>
2023-10-11 15:21:39 +0200

    #18 fix kalman filter

--- complete_unity/src/CANSender/SendSomeipThread.cpp
+++ complete_unity/src/CANSender/SendSomeipThread.cpp
@@ -63,8 +63,8 @@ void kalmanFilter_(double measuredstate, double estimation[SIZE], double letterP
 {
     double letterA[SIZE][SIZE] = {{1, dt},
                                   {0, 1}};
-    double letterQ[SIZE][SIZE] = {{5, 0},
-                                  {0, 5}};
+    double letterQ[SIZE][SIZE] = {{50, 0},
+                                  {0, 50}};
     double letterH[MEASURE_SIZE][SIZE] = {{1, 0}};
     double letterR[MEASURE_SIZE] = {25};
 
