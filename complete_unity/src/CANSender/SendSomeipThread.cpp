1ff0b9f991744ffed0217f4439a42a8ae632447e
lalywr2000 <12190450@inha.edu>
2023-10-11 13:34:57 +0200

    #18 fix kalman filter

--- complete_unity/src/CANSender/SendSomeipThread.cpp
+++ complete_unity/src/CANSender/SendSomeipThread.cpp
@@ -1,14 +1,6 @@
 #include "SendSomeipThread.hpp"
 
 
-double speed_sensor_estimation[SIZE] = {0, 0};
-double speed_sensor_letterP[SIZE][SIZE] = {{100, 0},
-                                           {0, 100}};
-double speed_sensor_dt = 1;
-double speed_sensor_renewed_e[SIZE], speed_sensor_renewed_P[SIZE][SIZE];
-double speed_sensor_measuredstate;
-
-
 void matrix_multiply(double A[SIZE][SIZE], double B[SIZE][SIZE], double result[SIZE][SIZE]) 
 {
     for (int i = 0; i < SIZE; i++) 
@@ -127,6 +119,13 @@ void *SendSomeipThread(void *arg)
     
     CommonAPI::CallStatus callStatus;
     std::string returnMessage;
+    
+    double speed_sensor_estimation[SIZE] = {0, 0};
+    double speed_sensor_letterP[SIZE][SIZE] = {{100, 0},
+                                               {0, 100}};
+    double speed_sensor_dt = 1;
+    double speed_sensor_renewed_e[SIZE], speed_sensor_renewed_P[SIZE][SIZE];
+    double speed_sensor_measuredstate;
     
     while (1)
     {
