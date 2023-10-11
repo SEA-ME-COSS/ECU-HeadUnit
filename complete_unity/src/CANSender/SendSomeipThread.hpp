f8084b57541cd03f1834519e35408cac7d41625b
lalywr2000 <12190450@inha.edu>
2023-10-11 13:24:19 +0200

    #18 fix kalman filter

--- complete_unity/src/CANSender/SendSomeipThread.hpp
+++ complete_unity/src/CANSender/SendSomeipThread.hpp
@@ -16,12 +16,6 @@
 #define MEASURE_SIZE 1
 
 
-double speed_sensor_estimation[SIZE];
-double speed_sensor_letterP[SIZE][SIZE];
-double speed_sensor_dt;
-double speed_sensor_renewed_e[SIZE], speed_sensor_renewed_P[SIZE][SIZE];
-double speed_sensor_measuredstate;
-
 void matrix_multiply(double A[SIZE][SIZE], double B[SIZE][SIZE], double result[SIZE][SIZE]);
 void kalmanFilter_(double measuredstate, double estimation[SIZE], double letterP[SIZE][SIZE], double dt, double renewed_e[SIZE], double renewed_P[SIZE][SIZE]);
 uint16_t filter(uint16_t speed_sensor_rpm);
