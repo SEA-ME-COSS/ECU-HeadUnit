1ee808f82ceabc56e70be7a8e70d5fadfb7f5704
lalywr2000 <12190450@inha.edu>
2023-10-11 10:48:10 +0200

    #18 fix kalman fifilter

--- complete_unity/src/CANSender/KalmanFilter.h
+++ complete_unity/src/CANSender/KalmanFilter.h
@@ -15,6 +15,13 @@ extern "C" {
 #define MEASURE_SIZE 1
 
 
+extern double speed_sensor_estimation[SIZE];
+extern double speed_sensor_letterP[SIZE][SIZE];
+extern double speed_sensor_dt;
+extern double speed_sensor_renewed_e[SIZE], speed_sensor_renewed_P[SIZE][SIZE];
+extern double speed_sensor_measuredstate;
+
+
 void matrix_multiply(double A[SIZE][SIZE], double B[SIZE][SIZE], double result[SIZE][SIZE]);
 void kalmanFilter_(double measuredstate, double estimation[SIZE], double letterP[SIZE][SIZE], double dt, double renewed_e[SIZE], double renewed_P[SIZE][SIZE]);
 uint16_t filter(uint16_t speed_sensor_rpm);
