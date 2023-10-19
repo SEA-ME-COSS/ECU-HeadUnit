9516e62daa68d0ed15f9f9b87b1286778db9b890
lalywr2000 <12190450@inha.edu>
2023-10-19 19:50:37 +0200

    #34 comment reboot script

--- reboot_script/reboot_can_sender.sh
+++ reboot_script/reboot_can_sender.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+# Define the process name to be checked and controlled
 process_name="CANSender"
 
+# Check if a process with the specified name is running
 if pgrep -x "$process_name" > /dev/null
 then
+  # If the process is running, gracefully terminate it using pkill
   pkill -x "$process_name"
 fi
 
+# Set up and configure the CAN interface 'can0'
+# Bring the 'can0' interface up and set its bitrate to 500,000 bits per second
 sudo ip link set can0 up type can bitrate 500000
+
+# Set the transmit queue length of 'can0' to 65,536 (maximum value)
 sudo ifconfig can0 txqueuelen 65536
 
+# Change the working directory to '../build'
 cd ../build
+
+# Run the specified process with '&' to execute it in the background
 ./"$process_name" &
 
--- reboot_script/reboot_instrument_cluster.sh
+++ reboot_script/reboot_instrument_cluster.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Define the process name to be checked and controlled
 process_name="InstrumentCluster"
 
+# Check if a process with the specified name is running
 if pgrep -x "$process_name" > /dev/null
 then
+  # If the process is running, gracefully terminate it using pkill
   pkill -x "$process_name"
 fi
 
+# Change the working directory to '../build'
 cd ../build
+
+# Run the specified process with '-platform linuxfb' and '&' to execute it in the background
 ./"$process_name" -platform linuxfb &
 
