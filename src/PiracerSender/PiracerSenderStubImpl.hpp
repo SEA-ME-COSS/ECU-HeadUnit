e569e64f7e4af60a64bdf3ca76556145f4997ca0
lalywr2000 <12190450@inha.edu>
2023-10-19 18:09:41 +0200

    #34 comment PiracerSender

--- src/PiracerSender/PiracerSenderStubImpl.hpp
+++ src/PiracerSender/PiracerSenderStubImpl.hpp
@@ -1,18 +1,18 @@
 #ifndef PIRACERSENDERSTUBIMPL_HPP
 #define PIRACERSENDERSTUBIMPL_HPP
 
-
 #include <CommonAPI/CommonAPI.hpp>
 #include <v1/commonapi/PiracerSenderStubDefault.hpp>
 
-
-class PiracerSenderStubImpl: public v1_0::commonapi::PiracerSenderStubDefault
-{
+// Declaration of the PiracerSenderStubImpl class
+class PiracerSenderStubImpl : public v1_0::commonapi::PiracerSenderStubDefault {
 public:
+    // Constructor for PiracerSenderStubImpl
     PiracerSenderStubImpl();
+
+    // Destructor for PiracerSenderStubImpl
     virtual ~PiracerSenderStubImpl();
 };
-
 
 #endif
 
