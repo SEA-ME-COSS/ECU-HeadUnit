5c11ef3ae7ea82e866c9b0ad1387adad52b5b711
lalywr2000 <12190450@inha.edu>
2023-10-25 15:01:11 +0200

    #39 rebuilding turn indicator auto off feature

--- src/PiracerController/PiracerController.cpp
+++ src/PiracerController/PiracerController.cpp
@@ -1,5 +1,6 @@
 #include <CommonAPI/CommonAPI.hpp>
 #include <v1/commonapi/IPCManagerProxy.hpp>
+#include <v1/commonapi/HeadUnitProxy.hpp>
 
 #include "ControllerClass.hpp"
 #include "PiracerControllerStubImpl.hpp"
@@ -12,6 +13,7 @@ int main()
     std::shared_ptr<CommonAPI::Runtime> runtime;
     std::shared_ptr<PiracerControllerStubImpl> PiracerControllerService;
     std::shared_ptr<IPCManagerProxy<>> IPCManagertargetProxy;
+    std::shared_ptr<HeadUnitProxy<>> HeadUnittargetProxy;
 
     runtime = CommonAPI::Runtime::get();
 
@@ -19,6 +21,7 @@ int main()
     PiracerControllerService = std::make_shared<PiracerControllerStubImpl>();
     runtime->registerService("local", "PiracerController", PiracerControllerService);
     IPCManagertargetProxy = runtime->buildProxy<IPCManagerProxy>("local", "IPCManager");
+    HeadUnittargetProxy = runtime->buildProxy<HeadUnitProxy>("local", "HeadUnit");
 
     // Initialize the controller class for interacting with the game controller
     ControllerClass controller;
@@ -34,6 +37,9 @@ int main()
 	// Send control data to IPCManager
         IPCManagertargetProxy->setThrottle(controller.getThrottle(), callStatus, returnMessage);
 	IPCManagertargetProxy->setSteering(controller.getSteering(), callStatus, returnMessage);
+	
+	// Send control data to HeadUnit
+	HeadUnittargetProxy->setSteering(controller.getSteering(), callStatus, returnMessage);
     }
 
     return 0;
