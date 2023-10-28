f6cd4b154970f38d17edb07cfb919177c54db0ac
lalywr2000 <12190450@inha.edu>
2023-10-28 14:03:27 +0200

    #39 gamepad gear

--- src/PiracerController/ControllerClass.cpp
+++ src/PiracerController/ControllerClass.cpp
@@ -56,10 +56,10 @@ void ControllerClass::readControl()
     pSteering = PyObject_GetAttrString(pInput, "analog_stick_left");
     pSteering = PyObject_GetAttrString(pSteering, "x");
     
-    pButton_A = PyObject_GetAttrString(pInput, "button_a");
+    /*pButton_A = PyObject_GetAttrString(pInput, "button_a");
     pButton_B = PyObject_GetAttrString(pInput, "button_b");
     pButton_X = PyObject_GetAttrString(pInput, "button_x");
-    pButton_Y = PyObject_GetAttrString(pInput, "button_y");
+    pButton_Y = PyObject_GetAttrString(pInput, "button_y");*/
     
     /*if (pButton_A == Py_True)
     {
