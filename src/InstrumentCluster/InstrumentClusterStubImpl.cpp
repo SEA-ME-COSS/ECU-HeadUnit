553e0266e77d00ab749302d63444c19ba154e285
lalywr2000 <12190450@inha.edu>
2023-12-04 16:32:41 +0100

    #14 edit comment

--- src/InstrumentCluster/InstrumentClusterStubImpl.cpp
+++ src/InstrumentCluster/InstrumentClusterStubImpl.cpp
@@ -1,22 +1,16 @@
 #include "InstrumentClusterStubImpl.hpp"
 
 InstrumentClusterStubImpl::InstrumentClusterStubImpl() { }
-
 InstrumentClusterStubImpl::~InstrumentClusterStubImpl() { }
 
 void InstrumentClusterStubImpl::setSpeedRpm(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _sensorRpm, setSpeedRpmReply_t _reply)
 {
-    // Convert sensor RPM to a format suitable for the instrument cluster.
     temp = (uint16_t)((float)_sensorRpm / 2.6);
-
-    // Update the RPM property in the carinfo object with the converted value.
     carinfo.setRpm(temp);
 
-    // Calculate the speed based on the converted RPM and update the property.
     temp = (uint16_t)((float)temp * 3.4);
     carinfo.setSpeed(temp);
 
-    // Respond to the client's request by invoking the reply callback.
     _reply("");
 
     return;
@@ -24,10 +18,8 @@ void InstrumentClusterStubImpl::setSpeedRpm(const std::shared_ptr<CommonAPI::Cli
 
 void InstrumentClusterStubImpl::setBattery(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _battery, setBatteryReply_t _reply)
 {
-    // Update the battery level property in the carinfo object.
     carinfo.setBattery(_battery);
 
-    // Respond to the client's request by invoking the reply callback.
     _reply("");
 
     return;
@@ -35,10 +27,8 @@ void InstrumentClusterStubImpl::setBattery(const std::shared_ptr<CommonAPI::Clie
 
 void InstrumentClusterStubImpl::setGear(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _gear, setGearReply_t _reply)
 {
-    // Update the gear mode property in the carinfo object.
     carinfo.setGear(_gear);
     
-    // Respond to the client's request by invoking the reply callback.
     _reply("");
     
     return;
@@ -46,10 +36,8 @@ void InstrumentClusterStubImpl::setGear(const std::shared_ptr<CommonAPI::ClientI
 
 void InstrumentClusterStubImpl::setDirection(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _direction, setDirectionReply_t _reply)
 {
-    // Update the direction property in the carinfo object.
     carinfo.setDirection(_direction);
 
-    // Respond to the client's request by invoking the reply callback.
     _reply("");
 
     return;
@@ -57,10 +45,8 @@ void InstrumentClusterStubImpl::setDirection(const std::shared_ptr<CommonAPI::Cl
 
 void InstrumentClusterStubImpl::setLight(const std::shared_ptr<CommonAPI::ClientId> _client, std::string _light, setLightReply_t _reply)
 {
-    // Update the light color property in the carinfo object.
     carinfo.setLight(QString::fromStdString(_light));
 
-    // Respond to the client's request by invoking the reply callback.
     _reply("");
 
     return;
--- src/InstrumentCluster/InstrumentClusterStubImpl.hpp
+++ src/InstrumentCluster/InstrumentClusterStubImpl.hpp
@@ -12,27 +12,18 @@
 class InstrumentClusterStubImpl : public v1_0::commonapi::InstrumentClusterStubDefault
 {
 private:
-    InstrumentClusterSenderClass sender; // Create an instance of InstrumentClusterSenderClass for communication.
+    InstrumentClusterSenderClass sender;
 
-    uint16_t temp; // Temporary variable used for calculations
+    uint16_t temp;
 
 public:
     InstrumentClusterStubImpl();
     virtual ~InstrumentClusterStubImpl();
     
-    // Handle a request to update the speed and RPM of the instrument cluster
     virtual void setSpeedRpm(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _sensorRpm, setSpeedRpmReply_t _return);
-    
-    // Handle a request to update the battery level of the instrument cluster
     virtual void setBattery(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _battery, setBatteryReply_t _return);
-    
-    // Handle a request to update the gear mode of the instrument cluster
     virtual void setGear(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _gear, setGearReply_t _return);
-    
-    // Handle a request to update the direction indicator of the instrument cluster
     virtual void setDirection(const std::shared_ptr<CommonAPI::ClientId> _client, uint16_t _direction, setDirectionReply_t _return);
-    
-    // Handle a request to update the light color of the instrument cluster
     virtual void setLight(const std::shared_ptr<CommonAPI::ClientId> _client, std::string _light, setLightReply_t _return);
 };
 
