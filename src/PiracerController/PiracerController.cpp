08d7d4bc9856661dd79508f2bc0158105f87225a
Sujong Ha <138571365+lalywr2000@users.noreply.github.com>
2023-12-04 18:04:43 +0100

    #14 Apply naming convention to target

--- src/PiracerController/PiracerController.cpp
+++ src/PiracerController/PiracerController.cpp
@@ -10,12 +10,12 @@ int main()
 {
     std::shared_ptr<CommonAPI::Runtime> runtime;
     std::shared_ptr<PiracerControllerStubImpl> PiracerControllerService;
-    std::shared_ptr<IPCManagerProxy<>> IPCManagertargetProxy;
+    std::shared_ptr<IPCManagerProxy<>> IPCManagerTargetProxy;
 
     runtime = CommonAPI::Runtime::get();
     PiracerControllerService = std::make_shared<PiracerControllerStubImpl>();
     runtime->registerService("local", "PiracerController", PiracerControllerService);
-    IPCManagertargetProxy = runtime->buildProxy<IPCManagerProxy>("local", "IPCManager");
+    IPCManagerTargetProxy = runtime->buildProxy<IPCManagerProxy>("local", "IPCManager");
 
     ControllerClass controller;
 
@@ -28,35 +28,35 @@ int main()
 
         if (controller.getPreThrottle() != controller.getThrottle())
         {
-            IPCManagertargetProxy->setThrottle(controller.getThrottle(), callStatus, returnMessage);
+            IPCManagerTargetProxy->setThrottle(controller.getThrottle(), callStatus, returnMessage);
         }
         if (controller.getPreSteering() != controller.getSteering())
         {
-            IPCManagertargetProxy->setSteering(controller.getSteering(), callStatus, returnMessage);
+            IPCManagerTargetProxy->setSteering(controller.getSteering(), callStatus, returnMessage);
         }
         if (controller.getPreButtonP() != controller.getButtonP())
         {
-            IPCManagertargetProxy->setGearMode(0, callStatus, returnMessage);
+            IPCManagerTargetProxy->setGearMode(0, callStatus, returnMessage);
         }
         if (controller.getPreButtonR() != controller.getButtonR())
         {
-            IPCManagertargetProxy->setGearMode(1, callStatus, returnMessage);
+            IPCManagerTargetProxy->setGearMode(1, callStatus, returnMessage);
         }
         if (controller.getPreButtonN() != controller.getButtonN())
         {
-            IPCManagertargetProxy->setGearMode(2, callStatus, returnMessage);
+            IPCManagerTargetProxy->setGearMode(2, callStatus, returnMessage);
         }
         if (controller.getPreButtonD() != controller.getButtonD())
         {
-            IPCManagertargetProxy->setGearMode(3, callStatus, returnMessage);
+            IPCManagerTargetProxy->setGearMode(3, callStatus, returnMessage);
         }
         if ((!controller.getPreButtonLeftTurn()) && (controller.getButtonLeftTurn()))
         {
-            IPCManagertargetProxy->setDirection(1, callStatus, returnMessage);
+            IPCManagerTargetProxy->setDirection(1, callStatus, returnMessage);
         }
         if ((!controller.getPreButtonRightTurn()) && (controller.getButtonRightTurn()))
         {
-            IPCManagertargetProxy->setDirection(2, callStatus, returnMessage);
+            IPCManagerTargetProxy->setDirection(2, callStatus, returnMessage);
         }
     }
 
