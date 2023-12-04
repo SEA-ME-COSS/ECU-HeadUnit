0ad454209773f75a9a6897b1208139410140c958
lalywr2000 <12190450@inha.edu>
2023-12-04 15:15:36 +0100

    #14 changing gamepad

--- src/IPCManager/PiracerClass.cpp
+++ src/IPCManager/PiracerClass.cpp
@@ -106,7 +106,7 @@ void PiracerClass::applyThrottle(double throttle)
         case 1: // R (Reverse)
 	    if (throttle <= 0)
 	    {
-	        PyObject_CallMethod(pInstance, "set_throttle_percent", "(f)", throttle * -0.3); // Call the Python method "set_throttle_percent" with the throttle value
+	        PyObject_CallMethod(pInstance, "set_throttle_percent", "(f)", throttle * 0.3); // Call the Python method "set_throttle_percent" with the throttle value
 	    }
 	    else
 	    {
@@ -121,7 +121,7 @@ void PiracerClass::applyThrottle(double throttle)
         case 3: // D (Drive)
 	    if (throttle >= 0)
 	    {
-      	        PyObject_CallMethod(pInstance, "set_throttle_percent", "(f)", throttle * -0.5); // Call the Python method "set_throttle_percent" with the throttle value
+      	        PyObject_CallMethod(pInstance, "set_throttle_percent", "(f)", throttle * 0.5); // Call the Python method "set_throttle_percent" with the throttle value
 	    }
 	    else
 	    {
